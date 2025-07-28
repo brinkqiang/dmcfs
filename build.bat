ca7fd11d7ea6ac5d904081ae9c982775ceecdba4
brinkqiang <brink.qiang@gmail.com>
2025-07-29 00:57:33 +0800

    add

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ cd build
 
 cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
 cd ..
 
 rem pause
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd build
 
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
-
+echo "cd build && ctest -C relwithdebinfo --output-on-failure --output-junit report.xml && cd .."
 cd ..
 
 # echo continue && read -n 1
